7dffeffe8a1b6dded2ebcb9cda692ade74f725aa
Tommy Youn <tommyyoun@users.noreply.github.com>
2023-10-29 01:21:09 -0400

    feat(skillmenu.cs): add skill HUD displays

--- Assets/Scripts/SkillMenu.cs
+++ Assets/Scripts/SkillMenu.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SkillMenu : MonoBehaviour
-
-   
 {
     public static bool Paused = false;
     public GameObject SkillMenuCanvas;
+    public GameObject SkillHUD;
     public RectTransform rectTransform;
     private Vector2 mousePos;
     private FrogInputSystem script;
+
+    private GameObject tempSkillObject;
+    private static float relativeSkillXPosition = 0;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -40,12 +44,21 @@ public class SkillMenu : MonoBehaviour
         }
         if (Input.GetMouseButtonUp(0) && RectTransformUtility.RectangleContainsScreenPoint(rectTransform, mousePos) && rectTransform.CompareTag("Resist") && script.skillPoints > 0) {
             //fill in logic for Resistance skill
+
+            // display on hud
+            displaySkill(0);
         }
         if (Input.GetMouseButtonUp(0) && RectTransformUtility.RectangleContainsScreenPoint(rectTransform, mousePos) && rectTransform.CompareTag("IncJump") && script.skillPoints > 0) {
             //fil in logic for Increased Jump skill
+
+            // display on hud
+            displaySkill(1);
         }
         if (Input.GetMouseButtonUp(0) && RectTransformUtility.RectangleContainsScreenPoint(rectTransform, mousePos) && rectTransform.CompareTag("AimAssist") && script.skillPoints > 0) {
             //fill in logic for Aim Assist skill
+
+            // display on hud
+            displaySkill(2);
         }
     }
 
@@ -60,5 +73,14 @@ public class SkillMenu : MonoBehaviour
         SkillMenuCanvas.SetActive(false); 
         Time.timeScale = 1.0f;
         Paused = false; 
+    }
+
+    void displaySkill(int child)
+    {
+        tempSkillObject = SkillHUD.transform.GetChild(child).gameObject;
+        tempSkillObject.SetActive(true);
+        tempSkillObject.transform.position = tempSkillObject.transform.position + new Vector3(relativeSkillXPosition, 0, 0);
+
+        relativeSkillXPosition = relativeSkillXPosition + 100;
     }
 }
