79eef1ad538bebcf03b4adb08b0e006c0b53ae2f
andryu14 <143534883+andryu14@users.noreply.github.com>
2023-11-15 14:59:22 -0800

    Updated UI  settings and Sound
    Upated UI on Main Menu and in the lvl
    Music Scripts to manage sounds and to play a sound

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    [SerializeField] private AudioSource musicSource, sfxSource;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        sfxSource.PlayOneShot(clip);
+    }
+
+    public void PlayMusic(AudioClip clip)
+    {
+        if (musicSource.clip != clip)
+        {
+            musicSource.clip = clip;
+            musicSource.Play();
+        }
+    }
+
+}
